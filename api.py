868df918a205bb1cfed6ed05f850a54cfb90c263
Idriss Neumann <idriss.neumann@comwork.io>
2020-12-12 22:06:54 +0100

    Asynchronous url

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 from subprocess import check_output
+from multiprocessing import Process
 import os
 
 app = Flask(__name__)
@@ -13,6 +14,21 @@ def get_script_output (cmd):
         return check_output(cmd, shell=True, text=True)
     except:
         return check_output(cmd, shell=True, universal_newlines=True)
+
+def run_cmd_async():
+    print("[run_cmd_async] output = {}".format(get_script_output(os.environ['API_CMD'])))
+
+class AsyncCmdApi(Resource):
+    def get(self):
+        async_process = Process( 
+            target=run_cmd_async,
+            daemon=True
+        )
+        async_process.start()
+        return {
+            'executed': True,
+            'async': True
+        }
 
 class CmdApi(Resource):
     def get(self):
@@ -30,9 +46,11 @@ class RootEndPoint(Resource):
 
 health_check_routes = ['/', '/health', '/health/']
 cmd_routes = ['/cmd', '/cmd-api', '/cmd/', '/cmd-api/']
+async_cmd_routes = ['/cmd/async', '/cmd-api/async', '/cmd/async/', '/cmd-api/async/']
 
 api.add_resource(RootEndPoint, *health_check_routes)
 api.add_resource(CmdApi, *cmd_routes)
+api.add_resource(AsyncCmdApi, *async_cmd_routes)
 
 if __name__ == '__main__':
     app.run()
