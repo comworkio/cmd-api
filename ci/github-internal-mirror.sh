aba0be51a93c790004718885abc465710820e14e
Idriss Neumann <idriss.neumann@comwork.io>
2021-01-31 10:04:55 +0100

    Add internal mirror

---
+++ ci/github-internal-mirror.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SRC_PATH="/home/centos/cmd-api/"
+TARGET_PATH="/home/centos/cmd-api-internal/"
+
+cd "${TARGET_PATH}" && git pull origin master || :
+cd "${SRC_PATH}" && git pull origin master || :
+
+for i in *; do
+  if [[ $i != "ci" ]] && [[ -d "${i}" ]]; then
+    rm -rf "${TARGET_PATH}/${i}"
+    cp -R "${i}" "${TARGET_PATH}"
+  fi
+done
+
+cp Dockerfile* "${TARGET_PATH}"
+cp docker-compose* "${TARGET_PATH}"
+cp README.md "${TARGET_PATH}"
+cp .gitignore "${TARGET_PATH}"
+
+commit_msg=$(git log -1 --pretty=%B|sed '$ d')
+[[ $commit_msg ]] || commit_msg="Automatic update"
+
+cd "${TARGET_PATH}" && git pull origin master || :
+sed -i "10,14d" README.md
+
+git add .
+git commit -m "${commit_msg}"
+git push origin master 
+
+exit 0
