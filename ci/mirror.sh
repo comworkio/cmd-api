691ed670ee841eb79aff44a1b929844e1cf0d205
Idriss Neumann <idriss.neumann@comwork.io>
2023-01-04 16:11:22 +0100

    Remove some mirrors and add dockerignore

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,7 +5,4 @@ REPO_PATH="/home/centos/cmd-api/"
 cd "${REPO_PATH}" && git pull origin master || :
 git push github master 
 git push pgitlab master
-git push internal master
-git push bitbucket master
-git push froggit master
 exit 0
